ee0887a5ce1ebb085f9d40e7dda892ead34ba674
Josip Valtner <josipvaltner@.com>
2025-09-29 07:59:46 +0200

    In progress

---
+++ Gozba_na_klik/Gozba_na_klik/Controllers/MealAddonsController.cs
@@ -0,0 +1,18 @@
+﻿using Gozba_na_klik.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gozba_na_klik.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MealAddonController : ControllerBase
+    {
+        //private readonly UsersDbRepository _usersRepository;
+        private readonly IUserService _userService;
+
+        public MealAddonController(IMealAddonService mealAddonService)
+        {
+            _mealAddonService = mealAddonService;
+        }
+    }
+}
---
+++ Gozba_na_klik/Gozba_na_klik/Repositories/MealsRepository.cs
@@ -0,0 +1,50 @@
+﻿using Gozba_na_klik.Models;
+
+namespace Gozba_na_klik.Repositories
+{
+    public class MealsDbRepository : IMealsRepository
+    {
+        private GozbaNaKlikDbContext _context;
+
+        public MealsDbRepository(GozbaNaKlikDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<User>> GetAllAsync()
+        {
+            return await _context.Users.ToListAsync();
+        }
+        public async Task<User?> GetByIdAsync(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+        public async Task<bool> ExistsAsync(int id)
+        {
+            return await _context.Users.AnyAsync(u => u.Id == id);
+        }
+        public async Task<User> AddAsync(User user)
+        {
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+            return user;
+        }
+        public async Task<User> UpdateAsync(User user)
+        {
+            _context.Users.Update(user);
+            await _context.SaveChangesAsync();
+            return user;
+        }
+        public async Task<bool> DeleteAsync(int id)
+        {
+            User user = await _context.Users.FindAsync(id);
+            if (user == null)
+            {
+                return false;
+            }
+
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+    }
+}
