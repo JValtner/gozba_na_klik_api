cf6e6e8e74954d794b8296a13fa0bee77df2e710
Josip Valtner <josipvaltner@.com>
2025-10-22 19:13:11 +0200

    Odkomentovan meal filter

--- Gozba_na_klik/Gozba_na_klik/Repositories/RestaurantDbRepository.cs
+++ Gozba_na_klik/Gozba_na_klik/Repositories/RestaurantDbRepository.cs
@@ -85,8 +85,7 @@ public class RestaurantDbRepository : IRestaurantRepository
             .Include(r => r.Owner)
             .Include(r => r.WorkSchedules)
             .Include(r => r.ClosedDates);
-        //Disabled zbog testiranja ne prikazuje restorane za koje nema definisano radno vreme tj smatra da ne rade
-        //query = FilterRestaurants(query, filter);
+        query = FilterRestaurants(query, filter);
         query = SortRestaurants(query, sortType);
 
         int count = await query.CountAsync();
